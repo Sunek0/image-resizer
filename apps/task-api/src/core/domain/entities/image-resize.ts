544e169b87c376f12a325931f45e7f31835dd178
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-10 23:58:02 +0200

    feature (task-api): Add lambda domain entity

---
+++ apps/task-api/src/core/domain/entities/image-resize.ts
@@ -0,0 +1,13 @@
+export class ImageResize {
+  status: string;
+  imageId: string;
+  imageBigId: string;
+  imageLittleId: string;
+
+  constructor(status: string, imageId: string, imageBigId: string, imageLittleId: string) {
+    this.status = status;
+    this.imageId = imageId;
+    this.imageBigId = imageBigId;
+    this.imageLittleId = imageLittleId;
+  }
+}
