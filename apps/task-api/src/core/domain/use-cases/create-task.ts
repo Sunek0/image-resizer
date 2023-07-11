3f73605914dc68740625a6996129e55e546f7d15
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-11 23:39:46 +0200

    feature(task-api): Add create task use case

--- apps/task-api/src/core/domain/use-cases/create-task.ts
+++ apps/task-api/src/core/domain/use-cases/create-task.ts
@@ -1,15 +1,28 @@
 import { Inject, Service } from 'typedi';
+import { parse } from 'path';
 import { Task } from '../entities/task';
 import { ITaskRepository } from '../../repositories/task.repository';
 import { ICreateTaskInput } from '../interfaces/create-task-input';
+import { IFileService } from '../../services/file.service';
 
 @Service()
 export class CreateTask {
-  constructor(@Inject('TaskRepository') private taskRepository: ITaskRepository) {}
+  constructor(
+    @Inject('TaskRepository') private taskRepository: ITaskRepository,
+    @Inject('FileService') private fileService: IFileService
+  ) {}
 
   async execute(input: ICreateTaskInput): Promise<Task> {
     const { path, status } = input;
-    const task = new Task(path, status);
+
+    const basePath = parse(path).base;
+
+    const fileExists = await this.fileService.fileExists(path);
+
+    if (!fileExists) {
+      throw new Error(`${basePath} not exists`)
+    }
+    const task = new Task(basePath, status);
     await this.taskRepository.add(task);
     return task;
   }
