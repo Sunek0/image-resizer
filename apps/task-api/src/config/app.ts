f793a82d4636e69967a4c914530fe7ba66c1f089
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-11 07:41:10 +0200

    refactor(task-api): remove some comments in app

--- apps/task-api/src/config/app.ts
+++ apps/task-api/src/config/app.ts
@@ -1,17 +1,10 @@
 import { logger } from './logger';
 import pinoHttp from 'pino-http';
 import express, { Express, Request, Response } from 'express';
-// import requestID from 'express-request-id';
-// import Router from 'express-promise-router';
 import { json, urlencoded } from 'body-parser';
 import helmet from 'helmet';
-// import * as http from 'http';
-// import { StatusCodes } from "http-status-codes";
 
 import { connectDatabase } from '../infrastructure/database/connection';
-// import { createTask } from '../application/controllers/task-post.controller';
-// import { getTask } from '../application/controllers/task-get.controller';
-
 
 const app: Express = express();
 
@@ -23,7 +16,6 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
-// app.use(requestID());
 
 // Connect to the database
 connectDatabase().then(async () => {
