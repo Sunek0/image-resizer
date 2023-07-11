fa0e49aa7d8e03d2ccd5131af83d00203c344f2d
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-11 22:24:50 +0200

    refactor(task-api): Change database implementation

---
+++ apps/task-api/src/infrastructure/database/dynamodb.ts
@@ -0,0 +1,37 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import config from 'config';
+import { Service } from 'typedi';
+
+// let databaseConnection: DynamoDB;
+@Service()
+export class DynamoDBDatabase {
+  public instance: DynamoDB
+
+  async connect(): Promise<void> {
+    const dbConfig = config.get('aws.dynamodb');
+    this.instance = new DynamoDB({
+      region: dbConfig.region,
+      credentials: {
+        accessKeyId: dbConfig.accessKeyId,
+        secretAccessKey: dbConfig.secretAccessKey
+      }
+    });
+
+    await this.instance;
+  }
+}
+// const connectDatabase = async (): Promise<DynamoDB> => {
+//   // return new DynamoDBClient({ region: databaseConfig.region });
+//   const dbConfig = config.get('aws.dynamodb');
+//   databaseConnection = new DynamoDB({
+//     region: dbConfig.region,
+//     credentials: {
+//       accessKeyId: dbConfig.accessKeyId,
+//       secretAccessKey: dbConfig.secretAccessKey
+//     }
+//   });
+
+//   return databaseConnection;
+// };
+
+// export { databaseConnection, connectDatabase };
