b7ca3adabf29fc0672e2815302f8f07fdb1019d5
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-12 01:25:13 +0200

    refactor(task-api): Remove unneeded dependency

--- apps/task-api/src/application/controllers/task-get.controller.ts
+++ apps/task-api/src/application/controllers/task-get.controller.ts
@@ -1,7 +1,6 @@
 import { Service } from 'typedi';
 import { Request, Response } from 'express';
 import { GetTask } from '../../core/domain/use-cases/get-task';
-import { IGetTaskInput } from '../dto/get-task.input';
 
 @Service()
 export class GetTaskController {
