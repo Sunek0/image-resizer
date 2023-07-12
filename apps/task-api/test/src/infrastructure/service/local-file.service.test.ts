0c37202c500097ceafe344248602cd7eb309490d
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-13 01:10:39 +0200

    test(task-api): Add local file service tests

---
+++ apps/task-api/test/src/infrastructure/service/local-file.service.test.ts
@@ -0,0 +1,83 @@
+import fs, { promises as fsp } from 'fs';
+import errors from 'common-errors';
+import { LocalFileService } from '../../../../src/infrastructure/services/local-file.service'
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('Domain get task use case unit tests', () => {
+  it('should fileExists return true if file exists', async () => {
+    const localFileService = new LocalFileService();
+    const statSpy = jest.spyOn(fsp, 'stat')
+      .mockResolvedValue(Promise.resolve(new fs.Stats()));
+
+    const fileExists = await localFileService.fileExists('foobar');
+
+    expect(statSpy).toHaveBeenCalledTimes(1);
+    expect(fileExists).toBeTruthy();
+  });
+
+  it('should fileExists return false if file not exists', async () => {
+    const localFileService = new LocalFileService();
+    const statSpy = jest.spyOn(fsp, 'stat')
+      .mockResolvedValue(Promise.reject());
+
+    const fileExists = await localFileService.fileExists('foobar');
+
+    expect(statSpy).toHaveBeenCalledTimes(1);
+    expect(fileExists).toBeFalsy();
+  });
+
+  it('should getReaderStream return stream', async () => {
+    const localFileService = new LocalFileService();
+    const createReadStreamSpy = jest.spyOn(fs, 'createReadStream')
+      .mockReturnThis();
+
+    const getReaderStream = localFileService.getReaderStream('foobar');
+
+    expect(createReadStreamSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should getReaderStream thrown IO error if fails', async () => {
+    const localFileService = new LocalFileService();
+    const createReadStreamSpy = jest.spyOn(fs, 'createReadStream')
+      .mockImplementation(() => {
+        throw new Error('');
+      });
+
+    try {
+      const getReaderStream = localFileService.getReaderStream('foobar');
+    }
+    catch (err: any) {
+      expect(createReadStreamSpy).toHaveBeenCalledTimes(1);
+      expect(err).toBeInstanceOf(errors.io.IOError);;
+    }
+  });
+
+  it('should getWritableStream return stream', async () => {
+    const localFileService = new LocalFileService();
+    const createWriteStreamSpy = jest.spyOn(fs, 'createWriteStream')
+      .mockReturnThis();
+
+    const getWritableStream = localFileService.getWritableStream('foobar');
+
+    expect(createWriteStreamSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should getWritableStream thrown IO error if fails', async () => {
+    const localFileService = new LocalFileService();
+    const createWriteStreamSpy = jest.spyOn(fs, 'createWriteStream')
+      .mockImplementation(() => {
+        throw new Error('');
+      });
+
+    try {
+      const getWritableStream = localFileService.getWritableStream('foobar');
+    }
+    catch (err: any) {
+      expect(createWriteStreamSpy).toHaveBeenCalledTimes(1);
+      expect(err).toBeInstanceOf(errors.io.IOError);;
+    }
+  });
+});
