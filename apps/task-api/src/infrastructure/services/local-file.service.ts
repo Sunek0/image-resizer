dd01c9f2b95b0750abbccd926f6015d896a7d431
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-13 01:13:49 +0200

    refactor(task-api): Improve error handling in local file service

--- apps/task-api/src/infrastructure/services/local-file.service.ts
+++ apps/task-api/src/infrastructure/services/local-file.service.ts
@@ -1,5 +1,7 @@
-import fs, { promises as fsp } from 'fs';
+import errors from 'common-errors';
 import { Service } from 'typedi';
+import fs, { promises as fsp } from 'fs';
+import { logger } from '../../config/logger';
 import { IFileService } from '../../core/services/file.service';
 
 @Service('FileService')
@@ -7,18 +9,34 @@ export class LocalFileService implements IFileService {
   async fileExists(path: string): Promise<boolean> {
     return fsp.stat(path)
       .then(() => true)
-      .catch((e) => false);
+      .catch(() => false);
   }
 
   getReaderStream(path: string): fs.ReadStream {
-    return fs.createReadStream(path);
+    try {
+      return fs.createReadStream(path);
+    }
+    catch (err: any) {
+      logger.error({ path, error: err.name }, "Can't create read stream");
+      throw new errors.io.IOError(`Can't create read stream to ${path}`);
+    }
   }
 
   getWritableStream(path: string): fs.WriteStream {
-    return fs.createWriteStream(path);
+    try {
+      return fs.createWriteStream(path);
+    }
+    catch (err: any) {
+      logger.error({ path, error: err.name }, "Can't create directory");
+      throw new errors.io.IOError(`Can't create write stream to ${path}`);
+    }
   }
 
   async createFolder(path: string, recursive: boolean = false): Promise<void> {
-    await fsp.mkdir(path, { recursive });
+    await fsp.mkdir(path, { recursive })
+      .catch((err: any) => {
+        logger.error({ path, error: err.name }, "Can't create directory");
+        throw new errors.io.IOError(`Can't create ${path}`);
+      });
   }
 }
