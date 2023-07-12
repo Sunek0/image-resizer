0bb1cd5ab5c89c7d6186c708bc10dabe88e659c8
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-13 01:13:17 +0200

    test(task-api): Change dummy file service mock

--- apps/task-api/test/mocks/services/dummy-file-service.ts
+++ apps/task-api/test/mocks/services/dummy-file-service.ts
@@ -1,25 +1,12 @@
 import { ReadStream, WriteStream, fstat } from "fs";
 import { IFileService } from "../../../src/core/services/file.service";
-import { Readable } from "stream";
 
 export class DummyFileService implements IFileService {
   fileExists(path: string): Promise<boolean> {
     return Promise.resolve(true);
   }
   getReaderStream(path: string): ReadStream {
-    let eventCount = 0;
-  const mockEventStream = new Readable({
-    objectMode: true,
-    read: function (size) {
-      if (eventCount < 10) {
-        eventCount = eventCount + 1;
-        return this.push({message: `event${eventCount}`})
-      } else {
-        return this.push(null);
-      }
-    }
-  });
-    return mockEventStream;
+    return new ReadStream();
   }
   getWritableStream(path: string): WriteStream {
     return new WriteStream();
