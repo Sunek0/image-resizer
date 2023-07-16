ea973465683aa7ba9666037b4a1585316b460e9f
Daniel Barreiro <hola@danielbarreiro.es>
2023-07-16 12:40:32 +0200

    refactor(task-api): Clean comments in infraectructure db

--- apps/task-api/src/infrastructure/database/dynamodb.ts
+++ apps/task-api/src/infrastructure/database/dynamodb.ts
@@ -2,7 +2,6 @@ import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import config from 'config';
 import { Service } from 'typedi';
 
-// let databaseConnection: DynamoDB;
 @Service()
 export class DynamoDBDatabase {
   public instance: DynamoDB;
@@ -20,18 +19,3 @@ export class DynamoDBDatabase {
     await this.instance;
   }
 }
-// const connectDatabase = async (): Promise<DynamoDB> => {
-//   // return new DynamoDBClient({ region: databaseConfig.region });
-//   const dbConfig = config.get('aws.dynamodb');
-//   databaseConnection = new DynamoDB({
-//     region: dbConfig.region,
-//     credentials: {
-//       accessKeyId: dbConfig.accessKeyId,
-//       secretAccessKey: dbConfig.secretAccessKey
-//     }
-//   });
-
-//   return databaseConnection;
-// };
-
-// export { databaseConnection, connectDatabase };
